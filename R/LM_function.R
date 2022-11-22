84df417d9c8b1c291af2ad8457294fe22d3f914a
Beau Powers <beau.powers@selu.edu>
2022-11-22 19:03:20 +0000

    try ag

--- R/LM_function.R
+++ R/LM_function.R
@@ -2,15 +2,13 @@
 #' 
 #' @param  numarical data
 #' 
-#' @return  carapace length ~body_depth lm plot
+#' @return  carapace length body_depth lm plot
 #' 
 #' 
-
-
-lm_function <- function(carapace_length, body_depth){
+#' 
+ lm_function <- function(carapace_length, body_depth){
 model_fit <- lm(carapace_length ~ body_depth, data = crabs)
 broom::tidy(model_fit)
-
 broom::augment(model_fit) -> augmented_fit
 qqnorm(augmented_fit$.resid)
 qqline(augmented_fit$.resid, col = "purple")
